1296acbf898324a995a0d48e8f2d2ab01065fb15
tangjiangxu <2287280228@qq.com>
2019-11-18 17:40:39 +0800

    .net core hello world第一次提交

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    //1. F5可以直接启动程序
+    class Program
+    {
+        //整个程序的入口
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            ////为了避免控制窗体一闪而过
+            //Console.Write("Press any key to continue...");
+            //Console.ReadKey(true);
+
+            Console.WriteLine("\nWhat is your name");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date: d} at {date:t}!");
+            Console.Write("\n Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
